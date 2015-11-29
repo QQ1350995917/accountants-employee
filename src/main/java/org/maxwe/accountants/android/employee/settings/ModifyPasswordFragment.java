1007f5d6a30c46a49792120d2d9e98b46cdbc9da
DingPengwei <www.dingpengwei@gmail.com>
2015-11-29 18:56:28 +0800

    pre settings

--- src/main/java/org/maxwe/accountants/android/employee/settings/ModifyPasswordFragment.java
+++ src/main/java/org/maxwe/accountants/android/employee/settings/ModifyPasswordFragment.java
@@ -5,18 +5,30 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageView;
 import org.maxwe.accountants.android.employee.R;
 
 /**
  * Created by dingpw on 11/28/15.
  */
-public class ModifyPasswordFragment extends Fragment {
+public class ModifyPasswordFragment extends Fragment implements View.OnClickListener{
     public static final String TAG = "ModifyPasswordFragment";
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View loginView = inflater.inflate(R.layout.fragment_modify_passwrod, container, false);
-
-        return loginView;
+        View modifyPasswordView = inflater.inflate(R.layout.fragment_modify_passwrod, container, false);
+        ImageView backView = (ImageView) modifyPasswordView.findViewById(R.id.title_back);
+        backView.setVisibility(View.VISIBLE);
+        backView.setOnClickListener(this);
+        return modifyPasswordView;
     }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (R.id.title_back == id){
+
+        }
+    }
+
+
 }
--- src/main/java/org/maxwe/accountants/android/employee/settings/SettingsActivity.java
+++ src/main/java/org/maxwe/accountants/android/employee/settings/SettingsActivity.java
@@ -1,12 +1,14 @@
 package org.maxwe.accountants.android.employee.settings;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.view.View;
-import android.widget.*;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import org.maxwe.accountants.android.employee.BaseActivity;
 import org.maxwe.accountants.android.employee.R;
-import org.maxwe.accountants.android.employee.access.LoginFragment;
+import org.maxwe.accountants.android.employee.access.AccessActivity;
 
 /**
  * Created by Pengwei Ding on 2015-11-25 14:23.
@@ -15,10 +17,15 @@ import org.maxwe.accountants.android.employee.access.LoginFragment;
  */
 public class SettingsActivity extends BaseActivity implements View.OnClickListener {
 
+    private LinearLayout settings_container;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_settings);
+
+        this.settings_container = (LinearLayout) this.findViewById(R.id.settings_container);
+
         ImageView backView = (ImageView) this.findViewById(R.id.title_back);
         backView.setVisibility(View.VISIBLE);
         backView.setOnClickListener(this);
@@ -32,11 +39,23 @@ public class SettingsActivity extends BaseActivity implements View.OnClickListen
     }
 
     @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+    }
+
+    @Override
     public void onClick(View v) {
         int id = v.getId();
-        if (R.id.settings_item_password_modify == id){
+        if (R.id.title_back == id) {
+            this.finish();
+        } else if (R.id.settings_item_password_modify == id) {
+            this.settings_container.removeAllViews();
             FragmentManager fragmentManager = this.getSupportFragmentManager();
-            fragmentManager.beginTransaction().replace(R.id.settings_container,new ModifyPasswordFragment(), ModifyPasswordFragment.TAG).commit();
+            fragmentManager.beginTransaction().add(R.id.settings_container,new ModifyPasswordFragment(), ModifyPasswordFragment.TAG).commit();
+        } else if (R.id.settings_item_logout == id) {
+            Intent intent = new Intent(this, AccessActivity.class);
+            this.startActivity(intent);
+            this.finish();
         }
 //        RelativeLayout item = (RelativeLayout) v;
 //        TextView childAt = (TextView) item.getChildAt(0);
